adf1f51ab040907b05c9c8a4d2bfb38c19828130
Manfred Touron <m@42.am>
2015-03-14 16:19:33 +0000

    WIP: porting to armhf

--- update.sh
+++ update.sh
@@ -3,14 +3,14 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-stage3="$(wget -qO- 'http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt' | tail -n1)"
+stage3="$(wget -qO- 'http://distfiles.gentoo.org/releases/arm/autobuilds/latest-stage3-armv7a_hardfp.txt' | tail -n1 | awk '{print $1}')"
 
 if [ -z "$stage3" ]; then
 	echo >&2 'wtf failure'
 	exit 1
 fi
 
-url="http://distfiles.gentoo.org/releases/amd64/autobuilds/$stage3"
+url="http://distfiles.gentoo.org/releases/arm/autobuilds/$stage3"
 name="$(basename "$stage3")"
 
 ( set -x; wget -N "$url" )
@@ -20,7 +20,8 @@ image="gentoo-temp:$base"
 container="gentoo-temp-$base"
 
 # bzcat thanks to https://code.google.com/p/go/issues/detail?id=7279
-( set -x; bzcat -p "$name" | docker import - "$image" )
+#( set -x; bzcat -p "$name" | docker import - "$image" )
+( set -x; cat "$name" | docker import - "$image" )
 
 docker rm -f "$container" > /dev/null 2>&1 || true
 ( set -x; docker run -t -v /usr/portage:/usr/portage:ro --name "$container" "$image" bash -exc $'
